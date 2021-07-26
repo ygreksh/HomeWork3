965ff007786ca9e70d04231247c5baf1e6efb092
ygreksh <ygreksh@gmail.com>
2021-07-26 23:23:57 +0300

    Add long double bool

--- PackAndUnpackTime/Program.cs
+++ PackAndUnpackTime/Program.cs
@@ -7,22 +7,67 @@ namespace PackAndUnpackTime
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("Hello World!");
             int i1 = 1;
-            object o;
+            long l1 = 2;
+            bool b1 = true;
+            double d1 = 3.14159;
+
+            object o1;
             Stopwatch stopwatch1 = new Stopwatch();
             Stopwatch stopwatch2 = new Stopwatch();
 
+            
+
+
+            stopwatch1.Reset();
             stopwatch1.Start();
-            o = i1;             //упаковка типа int в object
+            o1 = l1;             //упаковка типа long в object
             stopwatch1.Stop();
-            Console.WriteLine("Время упаковки = " + stopwatch1.ElapsedTicks + " тактов");
+            Console.WriteLine("Время упаковки типа long в object = " + stopwatch1.ElapsedTicks + " тактов");
 
-
+            stopwatch2.Reset();
             stopwatch2.Start();
-            int i2 = (int)o;    //распаковка из object  с явным приведением в тип int
+            long l2 = (long)o1;    //распаковка из object с явным приведением в тип long
             stopwatch2.Stop();
-            Console.WriteLine("Время распаковки = " + stopwatch2.ElapsedTicks + " тактов");
+            Console.WriteLine("Время распаковки типа object в long = " + stopwatch2.ElapsedTicks + " тактов");
+
+
+            stopwatch1.Reset();
+            stopwatch1.Start();
+            o1 = b1;             //упаковка типа bool в object
+            stopwatch1.Stop();
+            Console.WriteLine("Время упаковки типа bool в object = " + stopwatch1.ElapsedTicks + " тактов");
+
+            stopwatch2.Reset();
+            stopwatch2.Start();
+            bool b2 = (bool)o1;    //распаковка из object с явным приведением в тип bool
+            stopwatch2.Stop();
+            Console.WriteLine("Время распаковки типа object в bool = " + stopwatch2.ElapsedTicks + " тактов");
+
+            stopwatch1.Reset();
+            stopwatch1.Start();
+            o1 = d1;             //упаковка типа double в object
+            stopwatch1.Stop();
+            Console.WriteLine("Время упаковки типа double в object = " + stopwatch1.ElapsedTicks + " тактов");
+
+            stopwatch2.Reset();
+            stopwatch2.Start();
+            double d2 = (double)o1;    //распаковка из object с явным приведением в тип double
+            stopwatch2.Stop();
+            Console.WriteLine("Время распаковки типа object в double = " + stopwatch2.ElapsedTicks + " тактов");
+
+            stopwatch1.Reset();
+            stopwatch1.Start();
+            o1 = i1;             //упаковка типа int в object
+            stopwatch1.Stop();
+            Console.WriteLine("Время упаковки типа int в object = " + stopwatch1.ElapsedTicks + " тактов");
+
+            stopwatch2.Reset();
+            stopwatch2.Start();
+            int i2 = (int)o1;    //распаковка из object  с явным приведением в тип int
+            stopwatch2.Stop();
+            Console.WriteLine("Время распаковки типа object в int = " + stopwatch2.ElapsedTicks + " тактов");
+
         }
     }
 }
